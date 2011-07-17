74a436e20f5f3a4d8c5c5751db083f56f17477de
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-07-16 22:38:56 -0700

    refactor bot

--- lib/raibo/bot.rb
+++ lib/raibo/bot.rb
@@ -1,55 +1,55 @@
 module Raibo
   class Bot
-    attr_accessor :thread
-
     def initialize(*args)
+      @handlers = []
+
       if args.first.is_a?(Raibo::Connection)
         @connection = args.shift
-        @handlers = []
-
-        args.each do |handler|
-          append_handler(handler)
-        end
       else
         @connection = Raibo::Connection.new(*args)
-        @handlers = []
       end
     end
 
-    def insert_handler(handler=nil, &block)
-      @handlers.unshift(handler || block)
-    end
-
-    def append_handler(handler=nil, &block)
+    def use(handler=nil, &block)
       @handlers.push(handler || block)
     end
 
-    def run
-      @connection.open
-      @connection.handle_lines do |line|
-        begin
-          message = Raibo::Message.new(line)
-        rescue => e
-          if @connection.verbose
-            puts "Error parsing line:"
-            puts "  #{line}"
-            puts "  #{e.backtrace}"
-          end
-        end
-        @handlers.each do |handler|
-          break if handler.call(@connection, message)
-        end
+    def run(async=false)
+      if async
+        @thread = Thread.new { run_sync }
+        @thread.abort_on_exception = true
+      else
+        run_sync
       end
-    end
-
-    def run_async
-      @thread = Thread.new { self.run }
-      @thread.abort_on_exception = true
     end
 
     def stop
       @thread.kill if @thread
       @connection.close
     end
+
+    def alive?
+      @thread.alive? if @thread
+    end
+
+    private
+      def run_sync
+        @connection.open
+        @connection.handle_lines do |line|
+          begin
+            message = Raibo::Message.new(line)
+          rescue => e
+            if @connection.verbose
+              puts "Error parsing line:"
+              puts "  #{line}"
+              puts "  #{e.backtrace}"
+            end
+          end
+          @handlers.each do |handler|
+            break if handler.call(@connection, message)
+          end
+        end
+      end
+
   end
 end
